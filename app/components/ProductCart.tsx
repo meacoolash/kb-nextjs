16b01dc5b28b3f6d5a8d92084594dc8f9777e225
Miki Stec <mikulas@stec.sk>
2023-12-26 21:14:30 +0100

    CSS Module

--- app/components/ProductCart.tsx
+++ app/components/ProductCart.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import AddToCart from './AddToCart'
+import styles from './ProductCart.module.css'
 
 const ProductCart = () => {
     return (
-        <div>
+        <div className={styles.card}>
             <AddToCart />
         </div>
     )
